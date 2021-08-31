948fe7ca718f6ea7e227b137af0e8ff60574bcd1
Denis Solovev <denissolovev99@icloud.com>
2021-08-31 19:44:48 +0300

    update base_page, main_page, login_page, test_main_page, locators

--- conftest.py
+++ conftest.py
@@ -4,20 +4,18 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = "chrome"
+    user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart chrome browser for test..")
     browser = webdriver.Chrome(options=options)
-
-
     yield browser
     print("\nquit browser..")
     browser.quit()
+
